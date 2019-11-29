4002492fb337caaae0fb08fcd7033ace3f1a2bc4
VadosLight <it-vadim@inbox.ru>
2019-11-29 16:19:54 +0300

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using Steganography.Classes;
+
+
+namespace Steganography
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_ImportImage_Click(object sender, RoutedEventArgs e)
+        {
+            UserInterface userInterface = new UserInterface();
+        }
+
+        private void Btn_Cipher_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
