48b76d0f4f32af9725d468c67021b86dcecc77f9
VadosLight <it-vadim@inbox.ru>
2019-11-29 19:20:32 +0300

    1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
 using Steganography.Classes;
 
 
@@ -11,14 +15,46 @@ namespace Steganography
             InitializeComponent();
         }
 
+        public string typeOfFile = "";
+        public string imagePath = "";
+
         private void Btn_ImportImage_Click(object sender, RoutedEventArgs e)
         {
-            UserInterface userInterface = new UserInterface();
+            LoadImage();
         }
 
         private void Btn_Cipher_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void LoadImage()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpeg;*.jpg;*.bmp)|*.png;*.jpeg;*.jpg;*.bmp";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                imagePath = openFileDialog.FileName;
+                typeOfFile = TypeOfFile(imagePath).ToLower();
+                imgsource.Source = new BitmapImage(new Uri(imagePath));
+                //MessageBox.Show(typeOfFile);
+            }
+        }
+
+        public string TypeOfFile(string fileName)
+        {
+            return fileName.Substring(fileName.LastIndexOf(".") + 1);
         }
     }
 }
